39d9d779145e5015ca90abcad507232c3d456999
Viktor Deinyi <deinyiviktor@gmail.com>
2023-06-28 04:12:30 +0300

    Improve to task3 Add Task4 - 'http://the-internet.herokuapp.com'  testing

--- Task3.py
+++ Task3.py
@@ -1,21 +1,25 @@
-#try to create script with webdriver chrome which search via google softserve website and open it)
+# try to create script with webdriver chrome which search via google softserve website and open it)
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 url = "https://www.google.com/"
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.get(url=url)
-time.sleep(2)
-search_field = driver.find_element(by=By.NAME, value='q')
+driver.get(url)
+search_field = driver.find_element(By.NAME, 'q')
 search_field.send_keys("SoftServe")
 search_field.send_keys(Keys.ENTER)
-first_result = driver.find_element(by=By.XPATH, value='//*[@id="rso"]//div[1]/a/h3')
-first_result.click()
-time.sleep(2)
-
+try:
+    first_result = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="rso"]//div['
+                                                                                             '1]/a/h3')))
+    first_result.click()
+except:
+    print("Do not find element")
 driver.close()
 driver.quit()
